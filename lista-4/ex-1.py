bfb38c722c5e4732f49d365045081f127aa9c240
nunopalomino <nunopalomino@gmail.com>
2022-11-23 21:12:55 -0300

    ex 1 lista 4 - lista invertida

---
+++ lista-4/ex-1.py
@@ -0,0 +1,19 @@
+# Criar uma lista com os elementos['a', 'b', 'c', 'd', 'e', 'f', 'g']. Exibir o 3º, 5º e 6º
+# elemento, adicionar a string ‘pastel’ como 3º elemento, exibir a lista e
+# inverter os elementos da lista (lista reversa) e exibir a lista invertida.
+
+lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+print(lista[2])
+
+print(lista[5])
+
+print(lista[6])
+
+lista[2] = 'pastel'
+
+print(lista)
+
+lista.reverse()
+
+print(lista)
